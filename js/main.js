6a02cbfd20b835e99badfce14eea04f4852f9220
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2015-02-22 18:12:51 -0800

    Made changes to legal overlay pages

--- js/main.js
+++ js/main.js
@@ -1,30 +1,60 @@
 $(document).ready( function() {
 
-	$( '.close' ).click( function() {
-		
-	})
 
-	$( '#terms' ).click( function() {
-		$( '.close' ).click();
-		$(this).fadeOut(300);
-		$( '.ui-btn' ).text('');
+	$.tools.overlay.addEffect("fade", function(position, done) {
+		this.getOverlay().css(position).fadeIn(this.getConf().speed, done);
+	},
+
+	function(done) {
+		this.getOverlay().fadeOut(this.getConf().speed, done);
 	});
 
-	$( '#privacy' ).click( function() {
-		$( '.close' ).click();
-		$( this ).fadeOut(300);
-		$( '.ui-btn' ).text('');
-	})
+	$('a[rel]').each(function() {
+		$(this).overlay({
+			mask: 'black',
+			effect: 'fade',
+			top: 'center',
 
-	$(document).keyup( function(e) {
-		if ( e.keyCode == 27 ) {
-			$( '.close' ).click();
-			$( '.ui-btn' ).text('');
-		}
+			onBeforeLoad: function() {
+				var wrap = this.getOverlay().find('.content-wrap');
+				wrap.load(this.getTrigger().attr('href'));
+				
+			},
+			// // onLoad: function() {
+			// // 	var msk = $.mask;
+			// // 	if ( msk.isLoaded() != 'full' ) {
+			// // 		msk.load('black');
+			// // 		$('#terms').css('z-index', '100000000');
+			// // 		$('#privacy').css('z-index', '9999999');
+			// // 	}
+			// // },
+			// onClose: function() {
+			// 	//$.mask.close();
+			// },
+			// onLoad: function(){
+		 //        var t = $.mask;
+		 //        if(!t.isLoaded()){
+		 //            t.load();
+		 //            var ov = this.getOverlay();
+		 //            //ov.css('z-index', '9999');
+		 //        }
+		 //    },
+
+			oneInstance: true
+		});
+
 	});
 
-});
+	$('.terms-from-privacy').click(function(e) {
+	 	e.preventDefault();
+	 	$('#terms').css('z-index', '100000000');
+	 	$.mask.load('black');
+	});
 
-$(function() {
-	$( '.ui-btn' ).text('');
+	// $('.overlay').click(function() {
+	// 	$.mask.load('black');
+	// 	$('#privacy').css('z-index', '9999999');
+	// })
+
+
 });
